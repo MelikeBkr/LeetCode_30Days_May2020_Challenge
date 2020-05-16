265ec919515daf38b3555d26cf717f287d4fd6f9
MelikeBkr <melikeyaman.bakir@gmail.com>
2020-05-16 21:51:44 +0300

    Solution of Odd Even Linked List was pushed into master

---
+++ Week3/Solutions/OddEvenLinkedList.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode firstEven = even;
+
+        while (true)
+        {
+            if (odd == null || even == null || (even.next) == null)
+            {
+                odd.next = firstEven;
+                break;
+            }
+
+            odd.next = even.next;
+            odd = even.next;
+
+            if (odd.next == null)
+            {
+                even.next = null;
+                odd.next = firstEven;
+                break;
+            }
+            
+            even.next = odd.next;
+            even = odd.next;
+        }
+        return head;
+
+
+    }
+}
