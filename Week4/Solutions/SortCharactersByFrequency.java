f50cbcaa60ab7e537f14436bf5a36ef48f32a0c7
MelikeBkr <melikeyaman.bakir@gmail.com>
2020-05-23 14:07:00 +0300

    Comments were added for Sort Characters By Frequency

--- Week4/Solutions/SortCharactersByFrequency.java
+++ Week4/Solutions/SortCharactersByFrequency.java
@@ -4,8 +4,12 @@ class Solution {
         HashMap<Character, Integer> charFrequency = new HashMap<Character,Integer>();
         for (Character ch: s.toCharArray())
         {
+            //if hashmap does not contain ch add 1 (0+1) to the map
+            //if hashmap contains the value, increment value by 1
             charFrequency.put(ch,charFrequency.getOrDefault(ch,0)+1);
         }
+        //The lambda function will take two Integers as input parameters, subtract them from each other, 
+        //and return the arithmetic result. The lambda function implements the Functional Interface,                     //Comparator<T>
         PriorityQueue<Character> prioQueue = new PriorityQueue<>((a,b)->charFrequency.get(b)-charFrequency.get(a));
         prioQueue.addAll(charFrequency.keySet());
 
@@ -14,6 +18,7 @@ class Solution {
         while(!prioQueue.isEmpty())
         {
             char ch = prioQueue.remove();
+            //append ch number of frequency times
             for(int i=0; i<charFrequency.get(ch); i++)
             {
                 result.append(ch);
